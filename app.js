128a9158ae7b5e6e9b8d21151a9a1778072dde9d
Pawel <Pawel.Maciulewski@gmail.com>
2016-07-13 09:24:06 -0600

    include list authors feature

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var books = require('./routes/books');
+var authors = require('./routes/authors')
 // var detail = require('./routes/detail');
 require('dotenv').load();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/books', books);
+app.use('/authors', authors);
 // app.use('/detail', detail);
 
 // catch 404 and forward to error handler
--- routes/authors.js
+++ routes/authors.js
@@ -5,6 +5,24 @@ var api = require('../db/api');
 // Show list of books
 router.get('/', function(req, res, next) {
   api.listAuthors().then(function(authors) {
-    res.render('list-books', {authors: authors});
+    res.render('list-authors', {authors: authors});
   });
 });
+
+//DELETE book
+
+router.get('/delete/:id', function(req, res, next) {
+  api.getBook(req.params.id).then(function(book) {
+  res.render('delete-book', {book: book});
+  });
+});
+
+router.get('/delete/:id/end', function(req, res, next) {
+  api.deleteBook(req.params.id).then(function() {
+  res.redirect('/books');
+  });
+});
+
+
+
+module.exports = router;
